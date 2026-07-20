664fd75bb086420163f50c849d70d770605ba7fe
tappunk <me@tappunk.com>
2026-07-20 10:05:02 +0900

    chore: release v0.1.27 [skip ci]

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     {
       packages.aarch64-darwin.default = pkgs.rustPlatform.buildRustPackage {
         pname = "tnk";
-        version = "0.1.26";
+        version = "0.1.27";
         src = ./.;
 
         cargoLock = {

7de3c4f34bb1dea913d8342fca91199f334679a4
Richal Aleman <richalaleman@gmail.com>
2026-08-04 06:39:24 +0900

    chore: release v0.1.38

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     {
       packages.aarch64-darwin.default = pkgs.rustPlatform.buildRustPackage {
         pname = "tnk";
-        version = "0.1.37";
+        version = "0.1.38";
         src = ./.;
 
         cargoLock = {
